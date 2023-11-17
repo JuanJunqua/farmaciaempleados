bd2982da8c79c0984170453f70e24caea852caf5
unknown <jijunqua85@gmail.com>
2023-11-17 00:02:42 -0300

    editar eliminar etc

--- empleados/urls.py
+++ empleados/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('encargados/', views.encargados, name='encargados'),
     path('subencargados/', views.subencargados, name='subencargados'),
     path('mostradores/', views.mostradores, name='mostradores'),
-    path('mostrarempleados/', views.mostrarempleados, name='mostrarempleados')
+    path('mostrarempleados/', views.mostrarempleados, name='mostrarempleados'),
+    path('empleados/eliminar_empleado/<int:id>/', views.eliminar_empleado, name='eliminar_empleado'),
+    path('empleados/editar_empleado/<int:id>/', views.editar_empleado, name='editar_empleado'),
 ]
--- empleados/views.py
+++ empleados/views.py
@@ -5,6 +5,8 @@ from django.db.models import Q
 from functools import reduce
 from operator import or_
 
+from django.http import Http404
+
 
 def index(request):
     return render(request, 'base.html')
@@ -88,3 +90,49 @@ def mostrarempleados(request):
     }
 
     return render(request, 'mostrarempleados.html', context)
+
+
+
+def eliminar_empleado(request, id):
+    empleado_eliminar = (
+        encargado.objects.filter(id=id).first() or
+        subencargado.objects.filter(id=id).first() or
+        mostrador.objects.filter(id=id).first()
+    )
+
+    if request.method == "POST":
+        if empleado_eliminar:
+            empleado_eliminar.delete()
+            return redirect('mostrarempleados')
+
+    return render(request, 'eliminar_empleado.html', {'empleado': empleado_eliminar})
+
+def editar_empleado(request, id):
+    empleado = (
+        encargado.objects.filter(id=id).first() or
+        subencargado.objects.filter(id=id).first() or
+        mostrador.objects.filter(id=id).first()
+    )
+
+  
+
+    if request.method == "POST":
+        if isinstance(empleado, encargado):
+            form = EncargadoForm(request.POST, instance=empleado)
+        elif isinstance(empleado, subencargado):
+            form = SubencargadoForm(request.POST, instance=empleado)
+        elif isinstance(empleado, mostrador):
+            form = MostradorForm(request.POST, instance=empleado)
+
+        if form.is_valid():
+            form.save()
+            return redirect('mostrarempleados')
+    else:
+        if isinstance(empleado, encargado):
+            form = EncargadoForm(instance=empleado)
+        elif isinstance(empleado, subencargado):
+            form = SubencargadoForm(instance=empleado)
+        elif isinstance(empleado, mostrador):
+            form = MostradorForm(instance=empleado)
+
+    return render(request, 'editar_empleado.html', {'form': form, 'empleado': empleado})
